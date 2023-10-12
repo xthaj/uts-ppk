add700994cb7fb0f5ee542abe8952ec89c89f8f3
xthaj <athajuli@gmail.com>
2023-10-12 22:30:36 +0700

    base spring

---
+++ ppk/src/test/java/com/ses/ppk/SiSesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ses.ppk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiSesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
