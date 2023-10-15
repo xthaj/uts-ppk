90e3c92d8342b91b44b5c5210e1c2ed05a2936a5
xthaj <athajuli@gmail.com>
2023-10-15 18:23:40 +0700

    added previous fix on login & added documentation on authcontroller

--- ppk/src/main/java/com/ses/ppk/controller/AuthenticationController.java
+++ ppk/src/main/java/com/ses/ppk/controller/AuthenticationController.java
@@ -6,9 +6,14 @@ import com.ses.ppk.templates.AuthenticationRequest;
 import com.ses.ppk.templates.AuthenticationResponse;
 import com.ses.ppk.templates.RegisterRequest;
 import com.ses.ppk.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +25,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final AuthenticationService service;
     private final UserService userService;
+
+    @Operation(summary = "Register a new user")
+    @ApiResponse(responseCode = "200", description = "User registration successful",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomApiResponse.class))
+    )
+    @ApiResponse(responseCode = "409", description = "Username is not unique",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CustomApiResponse.class))
+    )
     @PostMapping("/register")
     public ResponseEntity<?> register (
             @RequestBody RegisterRequest request
@@ -32,10 +45,21 @@ public class AuthenticationController {
         }
     }
 
+    @Operation(summary = "Authenticate a user")
+    @ApiResponse(responseCode = "200", description = "Authentication successful", content = @Content(schema = @Schema(implementation = AuthenticationResponse.class)))
+    @ApiResponse(responseCode = "401", description = "Invalid credentials", content = @Content(schema = @Schema(implementation = CustomApiResponse.class)))
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(
+    public ResponseEntity<?> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
-        return ResponseEntity.ok(service.authenticate(request));
+        try {
+            AuthenticationResponse authenticationResponse = service.authenticate(request);
+
+            return ResponseEntity.ok(authenticationResponse);
+        } catch (BadCredentialsException e) {
+
+            CustomApiResponse errorResponse = new CustomApiResponse(HttpStatus.UNAUTHORIZED.value(), "Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+        }
     }
 }
