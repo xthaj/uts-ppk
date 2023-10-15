0f41093c8c1ae67185997de89e4ceca81af33f99
xthaj <athajuli@gmail.com>
2023-10-15 17:44:08 +0700

    + some info

---
+++ ppk/src/main/java/com/ses/ppk/config/OpenApiConfiguration.java
@@ -0,0 +1,18 @@
+package com.ses.ppk.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Atha Juli Riekawaty",
+                        email = "222111930@stis.ac.id"
+                ),
+                description = "SI-SES untuk UTS PPK",
+                title = "SI-SES"
+        )
+)
+public class OpenApiConfiguration {
+}
